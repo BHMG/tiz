bc6a7f21aa1b7645d448e5641bf4674e0b024fb9
unknown <l.mejia@uniandes.edu.co>
2019-08-15 18:42:51 -0500

    lab 1 parte 1

---
+++ temp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# p a c k a g e i m p o r t s
+import numpy as np
+import requests # f o r s a v i n g ima ge s f rom t h e i n t e r n e t
+import skimage.io as io
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+
+url="https://media.wired.com/photos/5a55457ef41e4c2cd9ee6cb5/master/w_2400,c_limit/Doggo-TopArt-104685145.jpg" 
+
+r = requests.get(url)
+with open('doggo', "wb") as f:
+    f.write(r.content)
+    
+doggo_img=io.imread('doggo')
+
+plt.imshow(doggo_img)
+plt.show()
