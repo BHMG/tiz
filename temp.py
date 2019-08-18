738353ecdbb3faa64b69086136de52dbd23b90fc
BHMG <bh.mejia@uniandes.edu.co>
2019-08-18 17:07:30 -0500

    No entiendo porque no sirve nada again

--- temp.py
+++ temp.py
@@ -4,11 +4,6 @@ Spyder Editor
 
 This is a temporary script file.
 
-Tiz tiene un pito
-Pichi es gay
-Leonardo es putaku y furro
-Miguel es lo mejor
-Tomy es tierno
 """
 
 # p a c k a g e i m p o r t s
@@ -17,14 +12,40 @@ import requests # f o r s a v i n g ima ge s f rom t h e i n t e r n e t
 import skimage.io as io
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
+from PIL import Image
 
 url="https://media.wired.com/photos/5a55457ef41e4c2cd9ee6cb5/master/w_2400,c_limit/Doggo-TopArt-104685145.jpg" 
 
 r = requests.get(url)
 with open('doggo', "wb") as f:
-    f.write(r.content)
+    f.write(r.content)  
     
-doggo_img=io.imread('doggo')
+doggo=io.imread('doggo')
 
-plt.imshow(doggo_img)
+plt.imshow(doggo)
+plt.subplot()
 plt.show()
+#plt.plot(3000,1600)
+#---------------------------------
+#cmap=plt.get.cmap(doggo)
+plt.subplot(2, 2,3)
+plt.imshow(doggo, cmap='gray')
+plt.title('Perro Gris')
+
+doggo_gris=rgb2gray('doggo')
+plt.savefig('Perro Gris')
+
+img = Image.open('doggo.PNG').convert('RGBA')
+a = np.array(img)
+plt.hist(a)
+
+
+
+
+
+
+
+
+
+
+
